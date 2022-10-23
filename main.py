a4aac93bf80102af0f704a973a4f5ba6acdba8c2
RayMek <116502408+RayMek@users.noreply.github.com>
2022-10-23 16:40:16 +0200

    Added initial website files

---
+++ main.py
@@ -0,0 +1,15 @@
+###server and clients
+### client (crowser) sends message to -> internet provider -> DNS server (looks into IP address) -> internet backbone
+### HTML: responsible for structure (add image, box, button...etc)
+### CSS: styling the website (fonts, colors ..etc)
+### java script: behaviour, function of website
+
+# Website = HTML + CSS + javascript
+
+### All browsers do is interpret the HTML, CSS, or javascrpt files into websites
+##HTML is the foundation of all websites!
+## HTML = Hypertext markup language
+
+###Boilerplate = code template
+
+##<p style="font-size:80%;"><strong>All photos are my own!</strong></strong></p>
